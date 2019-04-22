8de79b00bd854a2973da08ba54da1224bd285235
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2019-04-22 10:18:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 
 var read = require('read-data');
 var data =read.sync('data.json');
-for(i = 0; i < data.length; i++){
-    console.log(items['fname'], items['age']);
-}
+
+
+
+//console.log(JSON.stringify(data));
+
+//for(i = 0; i < data.length; i++){  //For loop for the array
+    //console.log(data['items']['fname'], data['items'][i]['age']);      //Prints out blank spots
+//}
+
+for(i = 0; i < data['items'].length; i++){// Read into the the items object instead of the file 
+    //console.log(JSON.stringify(items['fname'], items['age']));// Outputs but ends up outputing the entire object
+    console.log(data['items'][i]['fname'], data['items'][i]['age']);//console log the data
+
+}//End of for loop
